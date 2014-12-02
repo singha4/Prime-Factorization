25cf4b8ff3a37e850ae590fb9ddfc96177ea6061
singha4 <amandeep159632478@gmail.com>
2014-12-02 10:10:40 -0500

    Version 1

---
+++ Prime Factorization.java
@@ -0,0 +1,14 @@
+public class PrimeFactorization
+{
+  public static void main(String[] args)
+  {
+    
+  }
+  public static boolean isPrime(int num)
+  {
+    for(int i = 1; i < num; i++)
+    {
+      
+    }
+  }
+}
