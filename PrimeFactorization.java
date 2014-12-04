c058eb8efed18f85478975be5bf1434fa6495f6c
singha4 <amandeep159632478@gmail.com>
2014-12-04 12:36:35 -0500

    Final version 2
    now I'm done

--- PrimeFactorization.java
+++ PrimeFactorization.java
@@ -2,7 +2,14 @@ public class PrimeFactorization
 {
   public static void main(String[] args)
   {
-    System.out.println(primeFactorization(140));
+    if (isPrime(17) == true)
+    {
+      System.out.println("1 x " + 17);
+    }
+    else
+    {
+      System.out.println(primeFactorization(17));
+    }
   }
   
   public static String primeFactorization(int num)
@@ -22,5 +29,16 @@ public class PrimeFactorization
         }
     }
     return prime.substring(0, prime.length() - 2);
+  }
+  public static boolean isPrime(int num)
+  {
+    for (int i = 2; i < num; i++)
+    {
+      if (num % i == 0)
+      {
+        return false;
+      }
+    }
+    return true;
   }
 }
